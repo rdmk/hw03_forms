8c0b16df72334f4de0325cc4c73b9fbf285b539d
Erdem <munkueveg@gmail.com>
2021-10-12 02:39:07 +0800

    добавил файлы тестов в приложение posts

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,3 +1,4 @@
+from django.http import response
 from django.test import Client, TestCase
 
 
@@ -6,5 +7,13 @@ class StaticURLTests(TestCase):
         self.guest_client = Client()
 
     def test_homepage(self):
-        response = self.guest_client.get('/')  
+        response = self.guest_client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_author_page(self):
+        response = self.guest_client.get('/about/author/')
+        self.assertAlmostEqual(response.status_code, 200)
+
+    def test_tech_page(self):
+        response = self.guest_client.get('/about/tech/')
+        self.assertAlmostEqual(response.status_code, 200)
